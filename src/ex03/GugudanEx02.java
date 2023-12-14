6626c4d728aae4e8dd2b3ae7ac5fcd08572bb525
qkrehdrl4417 <qkrehdrl4417@naver.com>
2023-12-14 17:34:01 +0900

    구구단 연습

--- src/ex03/GugudanEx02.java
+++ src/ex03/GugudanEx02.java
@@ -4,14 +4,14 @@ public class GugudanEx02 {
         public static void main(String[] args) {
             // \t는 탭, \n은 엔터
 
-            for (int k = 2; k <= 9 ; k++){
-                System.out.print(k + "단"+ "\t\t\t");
+            for (int i = 2; i <= 9 ; i++){
+                System.out.print(i + "단"+ "\t\t\t");
             }
                 System.out.println();
-                for (int i = 1; i <= 9 ; i++) {
+                for (int j = 1; j <= 9 ; j++) {
 
                     for (int x = 2; x <= 9 ; x++) {
-                        System.out.print(x + "*" + i + "=" + x * i +"\t\t");
+                        System.out.print(x + "*" + j + "=" + x * j +"\t\t");
 
                 }
 
