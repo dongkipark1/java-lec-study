c467486ebdb7db77a378aeb6419c8d031d94bf82
qkrehdrl4417 <qkrehdrl4417@naver.com>
2023-12-14 17:26:49 +0900

    for구문 구구단

--- src/ex03/GugudanEx02.java
+++ src/ex03/GugudanEx02.java
@@ -4,8 +4,8 @@ public class GugudanEx02 {
         public static void main(String[] args) {
             // \t는 탭, \n은 엔터
 
-            for (int j = 2; j <= 9 ; j++){
-                System.out.print(j + "단"+ "\t\t\t");
+            for (int k = 2; k <= 9 ; k++){
+                System.out.print(k + "단"+ "\t\t\t");
             }
                 System.out.println();
                 for (int i = 1; i <= 9 ; i++) {
