4c08e136d3931220eb50c90b4db04781f63c35df
qkrehdrl4417 <qkrehdrl4417@naver.com>
2023-12-14 17:21:37 +0900

    배열 테테스트 01

--- src/ex03/GugudanEx02.java
+++ src/ex03/GugudanEx02.java
@@ -3,10 +3,16 @@ package ex03;
 public class GugudanEx02 {
         public static void main(String[] args) {
             // \t는 탭, \n은 엔터
-            for (int i = 1; i <= 9 ; i++) {
-                for (int x = 2; x <= 9 ; x++) {
 
-                    System.out.print(x + "*" + i + "=" + x * i +"\t");
+            for (int j = 2; j <= 9 ; j++){
+                System.out.print(j + "단"+ "\t\t\t");
+            }
+                System.out.println();
+                for (int i = 1; i <= 9 ; i++) {
+
+                    for (int x = 2; x <= 9 ; x++) {
+                        System.out.print(x + "*" + i + "=" + x * i +"\t\t");
+
                 }
 
                 System.out.println();
