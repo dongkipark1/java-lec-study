cbcc83a758db050a03b76c32cd03ee397bc824be
qkrehdrl4417 <qkrehdrl4417@naver.com>
2023-12-21 14:40:59 +0900

    bank app 완성 = 트랜잭션 , 객체상태변경, 객체상태확인

--- src/ex04/example/BankService.java
+++ src/ex04/example/BankService.java
@@ -5,11 +5,30 @@ import ex04.example.model.Account01;
 //트랜잭션 관리
 public class BankService {
 
+    public static void 출금(Account01 withdrawAccount, long amount) {
+        if (amount <= 0) {
+            System.out.println("0원이하 출금 불가");
+            return;
+        }
+        if (withdrawAccount.잔액부족하니(amount)) {
+            System.out.println("잔액 부족합니다");
+            return;
+        }
+        withdrawAccount.출금(amount);
+
+    }
+
     public static void 이체(Account01 senderAccount, Account01 receiverAccount, long amount) {
         if (amount <= 0) {
             System.out.println("0원이하 이체 불가");
             return;
         }
+
+        if (senderAccount.잔액부족하니(amount)) {
+            System.out.println("잔액 부족합니다");
+            return;
+        }
+
 
         senderAccount.출금(amount);
         receiverAccount.입금(amount);
--- src/ex04/example/model/Account01.java
+++ src/ex04/example/model/Account01.java
@@ -1,9 +1,18 @@
 package ex04.example.model;
 
+
+//객체의 상태를 변경, 객체의 상태를 확인
 public class Account01 {
     private final int id;
     private long balance; //balance의 상태가 바뀜
     private int userId;
+
+    public boolean 잔액부족하니(long amount) { //긍정으로 물어보면 좋은 코드
+        if (balance < amount) {
+            return true;
+        }
+        return false;
+    }
 
     //메서드는 하나의 책임만 가진다.(객체지향)
     public void 출금(long amount) { //출금은 출금만 하면된다 복잡하게 파라미터를 늘릴 필요없다
