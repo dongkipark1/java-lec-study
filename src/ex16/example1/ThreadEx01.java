9e7de1affc0f295c39f649cc9b7690ccbe96904c
qkrehdrl4417 <qkrehdrl4417@naver.com>
2024-01-03 17:47:49 +0900

    Thread basic

--- src/ex16/example1/ThreadEx01.java
+++ src/ex16/example1/ThreadEx01.java
@@ -2,8 +2,8 @@ package ex16.example1;
 
 public class ThreadEx01 {
 
-    public static void start1() {
-        for (int i = 0; i < 5; i++) {
+    public static void sub1() {
+        for (int i = 1; i <= 5; i++) {
             try {
                 System.out.println("start1 thread : " + i);
                 Thread.sleep(1000);
@@ -13,8 +13,8 @@ public class ThreadEx01 {
         }
     }
 
-    public static void start2() {
-        for (int i = 0; i < 5; i++) {
+    public static void sub2() {
+        for (int i = 1; i <= 5; i++) {
             try {
                 System.out.println("start2 thread : " + i);
                 Thread.sleep(1000);
@@ -24,14 +24,18 @@ public class ThreadEx01 {
         }
     }
 
-    //main Thread
+    // main thread
     public static void main(String[] args) {
-
         Thread t1 = new Thread(() -> {
-            start1();
+            sub1();
         });
         t1.start();
 
-        start2();
-    }
+        // 실을 만드는 것
+        Thread t2 = new Thread(() -> {
+            sub2();
+        });
+        t2.start(); // 실을 시작하는것
+    } // main 스레드 종료
 }
+
