d1d147e6f783e08a7a1b801f116cc14603282b41
qkrehdrl4417 <qkrehdrl4417@naver.com>
2024-01-11 14:16:21 +0900

    통신

--- src/ex17/multi/Client.java
+++ src/ex17/multi/Client.java
@@ -1,6 +1,8 @@
 package ex17.multi;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
@@ -12,17 +14,31 @@ public class Client {
             Socket socket = new Socket("localhost", 20000);
             Scanner sc = new Scanner(System.in);
             PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream())
+            );
 
-            // 2. 메시지 전송 스레드
+            // 2. 메시지 쓰기 스레드
             new Thread(() -> {
                 while (true) {
                     String keyboardMsg = sc.nextLine();
                     pw.println(keyboardMsg);
-
                 }
             }).start();
 
             // 3. 메시지 읽기 스레드
+
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        String requestMsg = br.readLine();
+                        System.out.println("서버로부터 받은 메시지 : " + requestMsg);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
 
         } catch (IOException e) {
             throw new RuntimeException(e);
--- src/ex17/multi/Server.java
+++ src/ex17/multi/Server.java
@@ -1,23 +1,23 @@
 package ex17.multi;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
     public static void main(String[] args) {
         try {
             // 1. 소켓과 버퍼 만들기
             ServerSocket serverSocket = new ServerSocket(20000); // 서버소켓, bind, listen
-            Socket socket = serverSocket.accept(); // accept(), listen(감지)하고 있음
-
+            Socket socket = serverSocket.accept(); // accept(), listen(감지)하고 있음 // 서버 확인
+            Scanner sc = new Scanner(System.in);
+            PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader br = new BufferedReader(
                     new InputStreamReader(socket.getInputStream())
             );
 
-            // 2. 메시지 받기 스레드
+            // 2. 메시지 읽기 스레드
             new Thread(() -> {
                 while (true) {
                     try {
@@ -29,6 +29,18 @@ public class Server {
                 }
             }).start();
 
+
+            // 3. 메시지 쓰기 스레드
+            BufferedWriter bw = new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream())
+            );
+            new Thread(() -> {
+                while (true) {
+                    String keyboardMsg = sc.nextLine();
+                    pw.println(keyboardMsg);
+
+                }
+            }).start();
 
         } catch (IOException e) {
             throw new RuntimeException(e);
